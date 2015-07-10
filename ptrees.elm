24292ffb0207375a37e8f52d38a749460cab8b45
Ruslan Shestopalyuk <rshestopalyuk@slb.com>
2015-07-10 20:27:13 +0200

    - coloring of the fruits according to index

--- ptrees.elm
+++ ptrees.elm
@@ -1,11 +1,11 @@
 module Ptree where
 
+import List
+import String
 import Color 
 import Time
-import List 
 import Window
 import Text
-import String
 
 import Signal exposing (..)
 import Graphics.Collage exposing (..)
@@ -14,65 +14,73 @@ import Graphics.Element exposing (..)
 goldenPhi: Float
 goldenPhi = 137.5/180.0*pi
 
-opt = { trunkClr = Color.hsl 0.3 0.5 0.4, 
-        trunkH = 80, trunkW1 = 3, trunkW2 = 2, rootH = 0, 
-        fruitHue = 1.5, fruitR = 20,
-        branchCone = 100, coneDamping = 0.95, 
-        nestScale = 0.8, 
-        budTreshold = 11, budSpread = 5, budOffs = 20, 
-        animDelay = 5, maxNum = 512 }
+-- tweakable options
+opt = { 
+    trunkClr = Color.hsl 0.3 0.3 0.5, 
+    trunkH = 80, trunkW1 = 3, trunkW2 = 2, rootH = 0, 
+    fruitHueScale = 0.1, fruitHueOffs = 1.5, fruitSaturation = 0.4, fruitLightness = 0.7,
+    fruitR = 20,
+    branchCone = 100, coneDamping = 0.95, 
+    nestScale = 0.8, 
+    budTreshold = 17, budSpread = 5, budOffs = 30, 
+    animDelay = 5, maxNum = 512 }
 
 trunk: Form
-trunk = polygon [(-opt.trunkW1, 0),
-                 (-opt.trunkW2, opt.trunkH),
-                 (opt.trunkW2, opt.trunkH), 
-                 (opt.trunkW1, 0),
-                 (0, -opt.rootH)]
-      |> filled opt.trunkClr
+trunk = 
+    polygon [(-opt.trunkW1, 0),
+             (-opt.trunkW2, opt.trunkH),
+             (opt.trunkW2, opt.trunkH), 
+             (opt.trunkW1, 0),
+             (0, -opt.rootH)]
+        |> filled opt.trunkClr
+
          
 fruit: Float -> Form
-fruit h = circle opt.fruitR
+fruit h = 
+    circle opt.fruitR
         |> gradient (fruitGrad h)
 
-branchAngle: Float -> Float -> Float -> Float
-branchAngle cone i n =
-  if n <= 1 then 0
-  else (degrees (i - (n - 1)/2)*cone/(n - 1))
 
 branchFork: Int -> Float -> List Int -> Int -> Form
 branchFork n cone xs i = 
     if n <= opt.budTreshold then
         -- a branch fork
-        [trunk, 
-         subTree cone xs 
-         |> moveY opt.trunkH 
-         |> scale opt.nestScale] 
-        |> group
-        |> rotate (branchAngle cone (toFloat i) (toFloat n))
+        let fn = toFloat n
+            fi = toFloat i
+            branchAngle = 
+                if n <= 1 then 0 else
+                    degrees (fi - (fn - 1)/2)*cone/(fn - 1)
+        in
+          [trunk, 
+           subTree cone xs i
+               |> moveY opt.trunkH 
+               |> scale opt.nestScale] 
+          |> group
+          |> rotate branchAngle
     else 
         -- a bud
         let ang = goldenPhi*(toFloat i)
             r = sqrt(ang)*opt.budSpread
-        in [subTree cone xs 
-            |> moveY r 
-            |> scale (opt.nestScale*0.5)] 
-           |> group 
-           |> rotate ang 
-           |> moveY opt.budOffs      
+        in 
+          [subTree cone xs i 
+              |> moveY r 
+              |> scale (opt.nestScale*0.5)] 
+          |> group 
+          |> rotate ang 
+          |> moveY opt.budOffs      
 
-subTree: Float -> List Int -> Form
-subTree cone s = 
+subTree: Float -> List Int -> Int -> Form
+subTree cone s i = 
   case s of
     n::xs -> [0..(n - 1)] 
      |> List.map (branchFork n (cone*opt.coneDamping) xs) 
      |> group
-    _ -> fruit opt.fruitHue
+    _ -> fruit (opt.fruitHueScale*(toFloat i) + opt.fruitHueOffs)
 
 
 ptree: List Int -> Element         
-ptree factors = collage 400 300 
-                [subTree opt.branchCone factors 
-                     |> moveY -150]
+ptree factors = 
+    collage 400 300 [subTree opt.branchCone factors 0 |> moveY -150]
 
 factorsString: List Int -> String
 factorsString factors = 
@@ -108,10 +116,9 @@ main =
 
 fruitGrad: Float -> Color.Gradient
 fruitGrad h = 
-    Color.radial (0, 0) 3 
-           (4, -4) 16
-           [(0, Color.hsl h 0.5 0.8), 
-            (1, Color.hsl h 0.5 0.5)]
+    Color.radial (0, 0) 5 (4, -4) 16
+             [(0, Color.hsl h opt.fruitSaturation (opt.fruitLightness + 0.2)), 
+            (1, Color.hsl h opt.fruitSaturation opt.fruitLightness)]
 
 primeFactors: Int -> Int -> List Int
 primeFactors n s = 
